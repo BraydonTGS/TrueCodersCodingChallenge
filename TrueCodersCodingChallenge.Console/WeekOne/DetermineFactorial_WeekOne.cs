9fd6173a8b7d21c546241ab181e1cfc596cadb95
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-07 17:19:48 -0600

    Week One Challenge Completed Backed with Unit Tests

---
+++ TrueCodersCodingChallenge.Console/WeekOne/DetermineFactorial_WeekOne.cs
@@ -0,0 +1,33 @@
+﻿namespace TrueCodersCodingChallenge.Console.WeekOne
+{
+    /// <summary>
+    /// Write a function to calculate the factorial of a given number.
+    /// 
+    /// The factorial of a non-negative integer n is the product of all positive integers less than or equal to n.
+    /// 
+    /// </summary>
+    public static class DetermineFactorial_WeekOne
+    {
+        /// <summary>
+        /// Determine the Factorial of a Number using Recursion.
+        /// 
+        /// Zero will always return 1
+        /// 
+        /// If the Value is less than zero, throw an ArgumentException as the Factorial is not Defined. 
+        /// 
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int DetermineFactorial(int num)
+        {
+            if (num < 0)
+                throw new ArgumentException("Factorial is not defined for negative numbers.");
+
+            if (num == 0)
+                return 1;
+
+            return num * DetermineFactorial(num - 1);
+        }
+
+    }
+}
---
+++ TrueCodersCodingChallenge.Tests/WeekOne/DetermineFactorial_WeekOne_Tests.cs
@@ -0,0 +1,56 @@
+﻿using TrueCodersCodingChallenge.Console.WeekOne;
+
+namespace TrueCodersCodingChallenge.Tests.WeekOne
+{
+    [TestClass]
+    public class DetermineFactorial_WeekOne_Tests
+    {
+
+        [TestMethod]
+        [DataRow(1, 1)]
+        [DataRow(2, 2)]
+        [DataRow(3, 6)]
+        [DataRow(4, 24)]
+        [DataRow(5, 120)]
+        [DataRow(6, 720)]
+        [DataRow(7, 5040)]
+        [DataRow(8, 40320)]
+        [DataRow(9, 362880)]
+        [DataRow(10, 3628800)]
+        public void CalculateTheFactorial_OfTheSpecifiedNumber_Success(int num, int expected)
+        {
+            var factorial = DetermineFactorial_WeekOne.DetermineFactorial(num);
+
+            Assert.IsNotNull(factorial);
+            Assert.AreEqual(expected, factorial);
+
+
+        }
+
+        [TestMethod]
+        [DataRow(0, 1)]
+        public void CalculateTheFactorial_OfTheSpecifiedNumber_NumberIsZero_ReturnsOne_Success(int num, int expected)
+        {
+            var factorial = DetermineFactorial_WeekOne.DetermineFactorial(num);
+
+            Assert.IsNotNull(factorial);
+            Assert.AreEqual(expected, factorial);
+        }
+
+        [TestMethod]
+        [DataRow(-1)]
+        [DataRow(-2)]
+        [DataRow(-3)]
+        [DataRow(-4)]
+        [DataRow(-5)]
+        [DataRow(-6)]
+        [DataRow(-7)]
+        [DataRow(-8)]
+        [DataRow(-9)]
+        [DataRow(-10)]
+        public void CalculateTheFactorial_OfTheSpecifiedNumber_NumberIsNegative_ThrowsArgumentException_Success(int num)
+        {
+            Assert.ThrowsException<ArgumentException>(() => DetermineFactorial_WeekOne.DetermineFactorial(num));
+        }
+    }
+}
