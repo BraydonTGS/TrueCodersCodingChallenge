f29090a4b0edefece2ee3ef9f1d1c42d45e57dad
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-21 16:26:33 -0600

    Created Two Binary Search Methods, One From Microsoft and One with Implementation. Added Tests

---
+++ TrueCodersCodingChallenge.Console/WeekThree/BinarySearchSortedArray_WeekThree.cs
@@ -0,0 +1,58 @@
+﻿namespace TrueCodersCodingChallenge.Console.WeekThree
+{
+    /// <summary>
+    ///  Notes on the Return Index: 
+    ///  The index of the specified value in the specified array, if value is found; otherwise, a negative number. 
+    ///  If value is not found and value is less than one or more elements in array, 
+    ///  the negative number returned is the bitwise complement of the index of the first element that
+    ///  is larger than value. If value is not found and value is greater than all elements in array, the negative number returned is the bitwise complement of (the index of the
+    ///  last element plus 1). 
+    ///  If this method is called with a non-sorted array, the return value can be incorrect and a negative number could be returned, even if value is present in array.
+    /// </summary>
+    public static class BinarySearchSortedArray_WeekThree
+    {
+
+        #region BinarySearchOnASortedArray
+        /// <summary>
+        /// Generic Binary Search On A Sorted Array
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="searchParams"></param>
+        /// <returns></returns>
+        public static int? BinarySearchOnASortedArray<T>(
+            Array array, T searchParams) =>
+            Array.BinarySearch(array, searchParams);
+        #endregion
+
+        #region BinarySearchImplementation
+        /// <summary>
+        /// Performs a binary search on a sorted array.
+        /// </summary>
+        public static int BinarySearchImplementation<T>(T[] array, T search) where T : IComparable<T>
+        {
+            var left = 0;
+            var right = array.Length - 1;
+
+            while (left <= right)
+            {
+                var median = (left + right) / 2;
+                var item = array[median];
+
+                var comparison = search.CompareTo(item);
+                if (comparison == 0)
+                    return median;
+
+                if (comparison < 0)
+                    right = median - 1;
+
+                else
+                    left = median + 1;
+            }
+
+            return -1;
+        }
+        #endregion
+
+    }
+}
---
+++ TrueCodersCodingChallenge.Tests/WeekThree/BinarySearchSortedArray_WeekThree_Tests.cs
@@ -0,0 +1,70 @@
+﻿using TrueCodersCodingChallenge.Console.WeekThree;
+using TrueCodersCodingChallenge.Console.WeekTwo;
+
+namespace TrueCodersCodingChallenge.Tests.WeekThree
+{
+    [TestClass]
+    public class BinarySearchSortedArray_WeekThree_Tests
+    {
+        [TestMethod]
+        public void BinarySearchOnASortedArray_Success()
+        {
+            var array = GenerateSortedArray();
+
+            var index = BinarySearchSortedArray_WeekThree.BinarySearchOnASortedArray(array, 8);
+
+            Assert.AreEqual(4, index);
+
+        }
+
+        [TestMethod]
+        public void BinarySearchOnASortedArray_ItemNotFound_ReturnsNegativeOne_Success()
+        {
+            var array = GenerateSortedArray();
+
+            var index = BinarySearchSortedArray_WeekThree.BinarySearchOnASortedArray(array, 1);
+
+            Assert.AreEqual(-1, index);
+
+        }
+
+        [TestMethod]
+        public void BinarySearchImplementation_Success()
+        {
+            var array = GenerateSortedArray();
+
+            var index = BinarySearchSortedArray_WeekThree.BinarySearchImplementation(array, 8);
+
+            Assert.AreEqual(4, index);
+
+        }
+
+        [TestMethod]
+        public void BinarySearchImplementation_ItemNotFound_ReturnsNegativeOne_Success()
+        {
+            var array = GenerateSortedArray();
+
+            var index = BinarySearchSortedArray_WeekThree.BinarySearchImplementation(array, 1);
+
+            Assert.AreEqual(-1, index);
+
+        }
+
+        public int[] GenerateSortedArray()
+        {
+            // Creates and initializes a new Array.
+            Array myIntArray = Array.CreateInstance(typeof(int), 5);
+
+            myIntArray.SetValue(8, 0);
+            myIntArray.SetValue(2, 1);
+            myIntArray.SetValue(6, 2);
+            myIntArray.SetValue(3, 3);
+            myIntArray.SetValue(7, 4);
+
+            // Do the required sort first
+            Array.Sort(myIntArray);
+
+            return (int[])myIntArray;
+        }
+    }
+}
